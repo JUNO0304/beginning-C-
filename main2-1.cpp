08155412d7f93fd383b1054b19522c85b60c0ef3
Junho <141983999+JUNO0304@users.noreply.github.com>
2025-02-27 13:19:18 +0900

    Create main2-1.cpp

---
+++ main2-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //전처리기에게 iostream 파일의 내용을 프로그램에 추가하라고 지시한다. 
+using namespace std; // using 지시자. 
+//myfirst.cpp 메세지를 출력한다. 
+
+int main()
+{
+    cout << "C++ 의 세계로 오십시오."; // 큰 따옴표 안에 있는 연속된 문자들을 (string) 이라고 부른다. 
+    cout << endl;
+    cout << "후회하지 않으실 겁니다." << endl;
+    return 0; 
+}
